84daa16747ebf6e17d0d7c08c470a5725957b49e
William Sulinski <w@williamsulinski.com>
2020-03-03 11:36:31 -0800

    Remove notnullable from tables

--- data/migrations/20200202072350_create-tables.js
+++ data/migrations/20200202072350_create-tables.js
@@ -8,20 +8,12 @@ exports.up = function (knex) {
             tbl.string('summary_date', 128)
                 .notNullable();
             tbl.integer('score_total')
-                .notNullable();
             tbl.string('bedtime_start')
-                .notNullable();
             tbl.integer('duration')
-                .notNullable();
             tbl.integer('readiness')
-                .notNullable();
             tbl.integer('hrv')
-                .notNullable();
             tbl.integer('rhr')
-                .notNullable();
             tbl.integer('weight')
-                .notNullable();
-
         });
 };
 
