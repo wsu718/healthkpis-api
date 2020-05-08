55bc12d7abadcdce7f1bc99382c3286b01ddae88
William Sulinski <w@williamsulinski.com>
2020-05-07 17:27:43 -0700

    Revert test

--- data/health-model.js
+++ data/health-model.js
@@ -59,17 +59,14 @@ function updateHealth(user_id, id, health) {
 function getWeeks(user_id) {
     return db('health')
         .where({ user_id })
-    // .groupBy('week_of_year').as('week')
-    // .avg({
-    //     'avg_score_total': 'score_total', 'avg_readiness': 'readiness', 'avg_duration': 'duration', 'avg_readiness': 'readiness', 'avg_hrv': 'hrv', 'avg_rhr': 'rhr', 'avg_weight': 'weight'
-    // })
+        .select('week_of_year')
+        .select('summary_date')
+        .groupBy('week_of_year').as('week')
+        .avg({
+            'avg_score_total': 'score_total', 'avg_readiness': 'readiness', 'avg_duration': 'duration', 'avg_readiness': 'readiness', 'avg_hrv': 'hrv', 'avg_rhr': 'rhr', 'avg_weight': 'weight'
+        })
+
     // .select('week_of_year').as('week_of_year')
-    // .select('summary_date')
-    //do I need .as on there?
+
 }
 
-// function getLeader(date){
-//     return db('health')
-//     .where({ date })
-
-// }
